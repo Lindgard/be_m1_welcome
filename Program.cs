cc8b7c20f8617030bc9c880f8f61cf168c82c831
ALP <a.b.lindgard@gmail.com>
2026-03-06 11:23:20 +0100

    started service-layer for business-logic, modified welcome-message in Program slightly for testing purposes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Hello, welcome to the message-generator");
+        Console.ResetColor();
     }
 }
