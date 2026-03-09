b66f83e000daebe12828f8e9cb6a822444ad6ff3
ALP <a.b.lindgard@gmail.com>
2026-03-09 10:26:26 +0100

    added basic functionality in responses that will be expanded upon, added DateTime usage that will be improved. Renamed var command to userInput for easier readability of intent

--- Program.cs
+++ Program.cs
@@ -7,5 +7,22 @@ class Program
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Hello, welcome to the message-generator");
         Console.ResetColor();
+        while (true)
+        {
+            Console.WriteLine("Enter name:");
+
+            var userInput = Console.ReadLine();
+            if (userInput != null && userInput.Equals("exit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Exiting program...");
+                Console.ResetColor();
+                break;
+            }
+            var currentDate = DateTime.Now;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Hello {userInput}\nThe time is: {currentDate}");
+            Console.ResetColor();
+        }
     }
 }
