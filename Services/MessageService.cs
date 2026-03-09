9224f159bbd42565506b5a8e13b0ce5b591cfbee
ALP <a.b.lindgard@gmail.com>
2026-03-09 10:28:01 +0100

    added a question-note for the future where I build the customizing methodology, removed the string variable

---
+++ Services/MessageService.cs
@@ -0,0 +1,10 @@
+namespace be_m1_welcome.Services;
+
+public class MessageService
+{
+    //*user input for use further down the file in other methods
+    //? hvordan flytte variabelen userInput hit fra inne i while-loop
+    //? i Program.cs og fortsatt la samme variabel bli brukt begge steder?
+    //* Business-logic for the welcome message
+    //* based on date and time from datetime in System.
+}
